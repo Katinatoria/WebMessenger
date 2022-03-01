65dd0bbe1ae86a54e0a14d06c67d4de2433a473a
Екатерина <@DESKTOP-6D01O7U>
2022-03-01 20:20:19 +0300

    Добавьте файлы проекта.

---
+++ Web_Messenger/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+ 
+namespace Web_Messenger
+{ 
+
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Send", message, userName);
+        }
+    }
+}
